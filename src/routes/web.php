e6919b80c44c1abc58af32ef46b378eeb4e087e4
Tsutomu Shinzawa <tsutomu.shinzawa320@gmail.com>
2025-12-23 14:46:32 +0900

    add register

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Requests\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +20,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('login', [AuthenticatedSessionController::class, 'store'])->middleware('email');
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->name('verification.notice');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    session()->get('unauthenticated_user')->sendEmailVerificationNotification();
+    session()->put('resent', true);
+    return back()->with('message', 'Verification link sent!');
+})->name('verification.send');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+    session()->forget('unauthenticated_user');
+    return redirect('/mypage/profile');
+})->name('verification.verify');
+
