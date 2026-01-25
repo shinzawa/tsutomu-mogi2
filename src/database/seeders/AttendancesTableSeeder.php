6cd474c02d73d7cc39efb22e51b553cbc89d2604
Tsutomu Shinzawa <tsutomu.shinzawa320@gmail.com>
2026-01-25 19:45:39 +0900

    seeding 1月分に拡張

--- src/database/seeders/AttendancesTableSeeder.php
+++ src/database/seeders/AttendancesTableSeeder.php
@@ -16,9 +16,185 @@ class AttendancesTableSeeder extends Seeder
     {
         $param = [
             'user_id' => 1,
-            'work_date' => '2026-01-25',
-            'clock_in' => '2026-01-25 08:00:00',
-            'clock_out' => '2026-01-25 18:00:00',
+            'work_date' => '2026-01-05',
+            'clock_in' => '2026-01-05 08:00:00',
+            'clock_out' => '2026-01-05 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-06',
+            'clock_in' => '2026-01-06 08:00:00',
+            'clock_out' => '2026-01-06 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-07',
+            'clock_in' => '2026-01-07 08:00:00',
+            'clock_out' => '2026-01-07 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-08',
+            'clock_in' => '2026-01-08 08:00:00',
+            'clock_out' => '2026-01-08 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-09',
+            'clock_in' => '2026-01-09 08:00:00',
+            'clock_out' => '2026-01-09 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-10',
+            'clock_in' => '2026-01-10 08:00:00',
+            'clock_out' => '2026-01-10 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-13',
+            'clock_in' => '2026-01-13 08:00:00',
+            'clock_out' => '2026-01-13 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-14',
+            'clock_in' => '2026-01-14 08:00:00',
+            'clock_out' => '2026-01-14 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-15',
+            'clock_in' => '2026-01-15 08:00:00',
+            'clock_out' => '2026-01-15 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-16',
+            'clock_in' => '2026-01-16 08:00:00',
+            'clock_out' => '2026-01-16 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-17',
+            'clock_in' => '2026-01-17 08:00:00',
+            'clock_out' => '2026-01-17 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-19',
+            'clock_in' => '2026-01-19 08:00:00',
+            'clock_out' => '2026-01-19 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-20',
+            'clock_in' => '2026-01-20 08:00:00',
+            'clock_out' => '2026-01-20 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-21',
+            'clock_in' => '2026-01-21 08:00:00',
+            'clock_out' => '2026-01-21 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-22',
+            'clock_in' => '2026-01-22 08:00:00',
+            'clock_out' => '2026-01-22 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-23',
+            'clock_in' => '2026-01-23 08:00:00',
+            'clock_out' => '2026-01-23 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-24',
+            'clock_in' => '2026-01-24 08:00:00',
+            'clock_out' => '2026-01-24 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-26',
+            'clock_in' => '2026-01-26 08:00:00',
+            'clock_out' => '2026-01-26 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-27',
+            'clock_in' => '2026-01-27 08:00:00',
+            'clock_out' => '2026-01-27 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-28',
+            'clock_in' => '2026-01-28 08:00:00',
+            'clock_out' => '2026-01-28 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-29',
+            'clock_in' => '2026-01-29 08:00:00',
+            'clock_out' => '2026-01-29 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-30',
+            'clock_in' => '2026-01-10 08:00:00',
+            'clock_out' => '2026-01-10 18:00:00',
+            'total_break_minutes' => '60',
+        ];
+        Attendance::create($param);
+        $param = [
+            'user_id' => 1,
+            'work_date' => '2026-01-31',
+            'clock_in' => '2026-01-10 08:00:00',
+            'clock_out' => '2026-01-10 18:00:00',
             'total_break_minutes' => '60',
         ];
         Attendance::create($param);
--- src/database/seeders/BreaksTableSeeder.php
+++ src/database/seeders/BreaksTableSeeder.php
@@ -14,11 +14,144 @@ class BreaksTableSeeder extends Seeder
      */
     public function run()
     {
+
         $param = [
             'attendances_id' => 1,
-            'break_start' => '2026-01-25 12:00:00',
-            'break_end' => '2026-01-25 13:00:00',
+            'break_start' => '2026-01-05 12:00:00',
+            'break_end' => '2026-01-05 13:00:00',
         ];
         BreakTime::create($param);
-    }
+        $param = [
+            'attendances_id' => 2,
+            'break_start' => '2026-01-06 12:00:00',
+            'break_end' => '2026-01-06 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 3,
+            'break_start' => '2026-01-07 12:00:00',
+            'break_end' => '2026-01-07 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 4,
+            'break_start' => '2026-01-08 12:00:00',
+            'break_end' => '2026-01-08 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 5,
+            'break_start' => '2026-01-09 12:00:00',
+            'break_end' => '2026-01-09 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 6,
+            'break_start' => '2026-01-10 12:00:00',
+            'break_end' => '2026-01-10 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 7,
+            'break_start' => '2026-01-13 12:00:00',
+            'break_end' => '2026-01-13 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 8,
+            'break_start' => '2026-01-14 12:00:00',
+            'break_end' => '2026-01-14 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 9,
+            'break_start' => '2026-01-15 12:00:00',
+            'break_end' => '2026-01-15 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 10,
+            'break_start' => '2026-01-16 12:00:00',
+            'break_end' => '2026-01-16 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 11,
+            'break_start' => '2026-01-17 12:00:00',
+            'break_end' => '2026-01-17 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 12,
+            'break_start' => '2026-01-19 12:00:00',
+            'break_end' => '2026-01-19 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 13,
+            'break_start' => '2026-01-20 12:00:00',
+            'break_end' => '2026-01-20 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 14,
+            'break_start' => '2026-01-21 12:00:00',
+            'break_end' => '2026-01-21 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 15,
+            'break_start' => '2026-01-22 12:00:00',
+            'break_end' => '2026-01-22 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 16,
+            'break_start' => '2026-01-23 12:00:00',
+            'break_end' => '2026-01-23 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 17,
+            'break_start' => '2026-01-24 12:00:00',
+            'break_end' => '2026-01-24 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 18,
+            'break_start' => '2026-01-26 12:00:00',
+            'break_end' => '2026-01-26 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 19,
+            'break_start' => '2026-01-27 12:00:00',
+            'break_end' => '2026-01-27 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 20,
+            'break_start' => '2026-01-28 12:00:00',
+            'break_end' => '2026-01-28 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 21,
+            'break_start' => '2026-01-29 12:00:00',
+            'break_end' => '2026-01-29 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 22,
+            'break_start' => '2026-01-30 12:00:00',
+            'break_end' => '2026-01-30 13:00:00',
+        ];
+        BreakTime::create($param);
+        $param = [
+            'attendances_id' => 23,
+            'break_start' => '2026-01-31 12:00:00',
+            'break_end' => '2026-01-31 13:00:00',
+        ];
+        BreakTime::create($param);
+            }
 }
